447225ad70da30cc029ea8e4ff4edfea6667f4c7
coala22 <proj.coala@gmail.com>
2022-12-03 10:03:02 -0300

    Criei a estrutura inicial das páginas.

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    # apps de terceiros
+    # apps do projeto
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
